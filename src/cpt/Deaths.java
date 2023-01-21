115c4e78e807bcbea3dc5a2d447bc373c8fc215a
StephanieHCTam <hoiching.tam23@ycdsbk12.ca>
2023-01-21 01:25:25 -0500

    create getter methods

---
+++ src/cpt/Deaths.java
@@ -0,0 +1,31 @@
+package cpt;
+
+public class Deaths {
+
+    private String entity;
+    private int year;
+    private String tuberculosis;
+
+    public String getEntity(){
+        return entity;
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public String getTuberculosis(){
+        return tuberculosis;
+    }
+
+    public Deaths(String entity, int year, String tuberculosis){
+        entity = this.entity;
+        year = this.year;
+        tuberculosis = this.tuberculosis;
+    }
+
+    public String toString(){
+        return entity + " " + year + " " + tuberculosis;
+    }
+    
+}
